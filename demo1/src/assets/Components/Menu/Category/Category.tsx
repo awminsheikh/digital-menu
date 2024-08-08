173327fea4afc9b7feb43afae1948ebfa27dc21e
awminsheikh <amin.sheikhazimi2212gmail.com>
2024-08-08 19:31:25 +0330

    correct categorie's name's font style

--- demo1/src/assets/Components/Menu/Category/Category.tsx
+++ demo1/src/assets/Components/Menu/Category/Category.tsx
@@ -10,7 +10,7 @@ const Category = ({id , name , image} : Props) => {
   return (
     <div key={id} className='flex flex-col items-center justify-center p-3 '>
         <img src={image} className='w-16' alt="" />
-        <p className="text-white ">{name}</p>
+        <p className={ id==1 ? "text-white font-mono font-bold" : "text-black font-mono font-bold"}>{name}</p>
     </div>
   )
 }
